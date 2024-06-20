302d5b09fd0d2f157d419bb98796fb7bb06e0a10
BraG29 <braianbarrios_29@hotmail.com>
2024-06-20 11:45:26 -0300

    pre-update

---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss'
+})
+export class LoginFormComponent {
+
+}
