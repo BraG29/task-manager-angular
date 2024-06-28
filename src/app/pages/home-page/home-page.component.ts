630e1c4cb121ca6cce105ea987df773f1b18e925
BraG29 <braianbarrios_29@hotmail.com>
2024-06-28 18:22:47 -0300

    home-page rendering navbar

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,25 +1,29 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {Router, RouterOutlet} from "@angular/router";
+import {NavComponent} from "../../components/nav/nav.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    NavComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent implements OnInit{
+export class HomePageComponent{
   token: string | null = null;
 
   constructor(private router: Router) {
   }
 
-  ngOnInit(): void {
-    this.token = sessionStorage.getItem('token');
-
-    if(!this.token){
-      this.router.navigate(['/login']);
-    }
-  }
+  // ngOnInit(): void {
+  //   this.token = sessionStorage.getItem('token');
+  //
+  //   if(!this.token){
+  //     this.router.navigate(['/login']);
+  //   }
+  // }
 
 }
